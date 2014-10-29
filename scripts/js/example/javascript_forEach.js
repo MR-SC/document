2ad559d4ef23686be4cf1298f1edb0ad28bf9012
MRSC <cloud760607@hotmail.com>
2014-10-30 04:34:28 +0800

    add example code
    Signed-off-by: MRSC <cloud760607@hotmail.com>

---
+++ scripts/js/example/javascript_forEach.js
@@ -0,0 +1,11 @@
+function logArrayElements(element, index, array) {
+	//	  console.log('a[' + index + '] = ' + element);
+	console.log(array);
+
+}
+// Note ellision, there is no member at 2 so it isn't visited
+ [2, 5,0 , 9].forEach(logArrayElements);
+// // logs:
+// // a[0] = 2
+// // a[1] = 5
+// // a[3] = 9
